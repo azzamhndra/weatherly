512231b4b35fb3eaf469f83577849f7ef3dcbfd9
Azza Mahendra <176836552+azzamhndra@users.noreply.github.com>
2026-08-06 16:17:24 +0700

    chore: init shadcn with custom preset

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Button variant={'default'}>Cari Kota</Button>
+    </div>
+  );
 };
 export default App;
